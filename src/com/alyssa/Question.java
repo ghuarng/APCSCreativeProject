b37943ec59d7a98334020fce25c5df4a799e94a4
asdf <gary0928@gmail.com>
2018-03-09 10:55:27 -0800

    Added pictures
    :ok_hand:  made you look

--- src/com/alyssa/Question.java
+++ src/com/alyssa/Question.java
@@ -1,8 +1,13 @@
-package com.alyssa;
+ 
 
 public class Question {
     int qNum;
+    String answer;
     public Question(int qNum) {
         this.qNum = qNum;
+        
+    }
+    public boolean checkCorrect(int response){
+        
     }
 }
