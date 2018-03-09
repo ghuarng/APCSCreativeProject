dc0811e2a085301b7dc55b6898d468317389cada
alyssathomason <34072898+alyssathomason@users.noreply.github.com>
2018-03-09 10:49:34 -0800

    erger
    ERGERBER

--- src/com/alyssa/Game.java
+++ src/com/alyssa/Game.java
@@ -5,35 +5,9 @@ import javax.swing.*;
 import javax.swing.plaf.metal.MetalButtonUI;
 
 public class Game extends JFrame implements ActionListener {
-    public boolean inGame;
-    final int X = 600, Y = 1024;
 
-    private JFrame window;
-    private JMenuBar mnuMain;
-    private JMenuItem   mnuClearGame, mnuGameTitle, mnuStartGame, mnuExit;
-
-
-    private Font fontToken = new Font("Arial", Font.BOLD, 70);
-    private Font fontRadio = new Font("Arial", Font.BOLD, 25);
-    private Font fontMenu = new Font("Arial", Font.BOLD, 18);
 
     public void init() {
-        inGame = false;
-
-        window = new JFrame("Your Mom's Tic Tac Toe Game");
-
-        mnuMain = new JMenuBar();
-        mnuClearGame = new JMenuItem("  Clear Game");
-        mnuGameTitle = new JMenuItem("Tic Tac Toe  ");
-        mnuStartGame = new JMenuItem(" Start Game");
-        mnuExit = new JMenuItem("    Quit");
-
-        window.setSize(X, Y);
-        window.setLocation(300, 180);
-        window.setResizable(true);
-        window.setLayout(new BorderLayout());
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 
     }
 
