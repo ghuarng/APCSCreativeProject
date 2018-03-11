43daf6b384268fc4fa10787c372a443ad477cea9
alyssathomason <34072898+alyssathomason@users.noreply.github.com>
2018-03-11 15:19:22 -0700

    3/11
    rbgb rg rgw

--- src/com/alyssa/Game.java
+++ src/com/alyssa/Game.java
@@ -11,14 +11,15 @@ public class Game extends JFrame implements ActionListener {
 
     private JFrame window;
     private JMenuBar mnuMain;
+    private JButton startGameButton;
     private JLabel mainTitle1, mainTitle2, mainTitle3, mainTitle4;
-    private JMenuItem   mnuRestart, mnuGameTitle, mnuStartQuiz, mnuExit;
+    private JMenuItem   mnuClearQuiz, mnuGameTitle, mnuStartQuiz, mnuExit;
 
     JPanel pnlBar, pnlGame, pnlTitle, pnlTitlePicture, pnlTitlePage, pnlQuestion, pnlPicture, pnlAnswer;
 
-    private Font fontToken = new Font("Arial", Font.BOLD, 70);
+    private Font fontToken = new Font("Impact", Font.BOLD, 70);
     private Font fontTitle = new Font("Impact", Font.BOLD, 100);
-    private Font fontMenu = new Font("Arial", Font.BOLD, 18);
+    private Font fontMenu = new Font("Impact", Font.BOLD, 18);
 
     public void init() {
         inGame = false;
@@ -26,8 +27,8 @@ public class Game extends JFrame implements ActionListener {
         window = new JFrame("|||||||||||||||||||REVENGERS REUNITE|||||||||||||||||||");
 
         mnuMain = new JMenuBar();
-        mnuRestart = new JMenuItem("  TRY AGAIN");
-        mnuGameTitle = new JMenuItem("QUIZ  ");
+        mnuGameTitle = new JMenuItem("HOME  ");
+        mnuClearQuiz = new JMenuItem("  CLEAR QUIZ");
         mnuStartQuiz = new JMenuItem(" START QUIZ");
         mnuExit = new JMenuItem("    GIVE UP");
 
@@ -48,45 +49,48 @@ public class Game extends JFrame implements ActionListener {
 
         //start menu initialization
         mnuMain.add(mnuGameTitle);
-        mnuGameTitle.setEnabled(false);
         mnuGameTitle.setFont(fontMenu);
-        mnuMain.add(mnuRestart);
-        mnuRestart.setFont(fontMenu);
+        mnuMain.add(mnuClearQuiz);
+        mnuClearQuiz.setFont(fontMenu);
         mnuMain.add(mnuStartQuiz);
         mnuStartQuiz.setFont(fontMenu);
         mnuMain.add(mnuExit);
         mnuExit.setFont(fontMenu);
         // adding Action Listener to all the Buttons and Menu Items
-        mnuRestart.addActionListener(this);
+        mnuClearQuiz.addActionListener(this);
         mnuExit.addActionListener(this);
-        mnuRestart.addActionListener(this);
+        mnuClearQuiz.addActionListener(this);
         //adding menu to panel
         pnlBar.add(mnuMain);
         pnlBar.setBackground(new Color(75, 255, 0));
         //end menu initialization;
 
         //title page
-        pnlTitlePage.setLayout(new FlowLayout(FlowLayout.CENTER));
-        pnlTitlePage.setBackground(new Color(69, 61, 0));
+        pnlTitlePage.setLayout(new GridLayout(X, Y - 180, 2, 2));
+        pnlTitlePage.setBackground(new Color(69, 26, 0));
         mainTitle1 = new JLabel("wElCOmE");
         mainTitle1.setFont(fontTitle);
-        pnlTitle.add(mainTitle1, BorderLayout.NORTH);
+        pnlTitle.add(mainTitle1);
         mainTitle2 = new JLabel("tO ThE");
         mainTitle2.setFont(fontTitle);
-        pnlTitle.add(mainTitle2, BorderLayout.CENTER);
+        pnlTitle.add(mainTitle2);
         mainTitle3 = new JLabel("rEveNgErS");
         mainTitle3.setFont(fontTitle);
-        pnlTitle.add(mainTitle3, BorderLayout.CENTER);
+        pnlTitle.add(mainTitle3);
         mainTitle4 = new JLabel("QuIz");
         mainTitle4.setFont(fontTitle);
-        pnlTitle.add(mainTitle4, BorderLayout.SOUTH);
+        pnlTitle.add(mainTitle4);
         pnlTitlePage.add(pnlTitle);
+        startGameButton = new JButton("Start Game");
+        startGameButton.setFont(fontTitle);
+        startGameButton.addActionListener(this);
+        pnlTitlePage.add(startGameButton);
         //end title page
 
         pnlGame.setLayout(new GridLayout(X, Y - 180, 2, 2));
         pnlGame.setBackground(new Color(255, 42, 0)); //background behind buttons
 
-        showTitlePage();
+        goToTitle();
 
         window.add(pnlBar, BorderLayout.NORTH);
         window.add(pnlGame, BorderLayout.CENTER);
@@ -96,25 +100,31 @@ public class Game extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent click) {
         // get the mouse click from the user
         Object source = click.getSource();
-
-        if (source == mnuRestart) {
+        if (source == mnuGameTitle) {
+            goToTitle();
+        } else if (source == mnuClearQuiz) {
             showGame();
+            /*startGame();*/
         } else if (source == mnuExit) {
 //            exitGame();
         } else if (source == mnuStartQuiz) {
             showGame();
-        } /*else if (source == startGameButton) {
-            startGame();*/
+            /*startGame();*/
+        } else if (source == startGameButton) {
+            showGame();
+            // startGame();
+        }
     }
 
     //methods for what the game will look like
     public void showTitlePage() {
-        pnlGame.setLayout(new BorderLayout());
-        pnlGame.add(pnlTitlePage, BorderLayout.CENTER);
+        pnlGame.add(pnlTitlePage);
         pnlTitlePage.requestFocus();
+        pnlGame.revalidate();
     }
     public void showGame() {
         pnlGame.remove(pnlTitlePage);
+        window.add(pnlGame, BorderLayout.CENTER);
         pnlGame.setLayout(new BorderLayout());
         pnlGame.add(pnlQuestion, BorderLayout.NORTH);
         pnlGame.add(pnlPicture, BorderLayout.CENTER);
